da263602b3073b817c98b6425edcefaa7543de1c
Данилов Александр <kss6655@mail.ru>
2023-04-24 19:13:26 +0500

    review fixes. Ready to check

--- HomeworkTests.cs
+++ HomeworkTests.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -35,8 +34,8 @@ public class HomeworkTests
         password.SendKeys("Zerx14589652.");
         var enter = driver.FindElement(By.Name("button"));
         enter.Click();
-        
-        wait.Until(x => x.FindElement(By.CssSelector("[data-tid='Title']")));
+
+        wait.Until(x => x.Url == $"{staffUrl}/news");
     }
 
     [Test]
@@ -87,12 +86,19 @@ public class HomeworkTests
         var sendButton = driver.FindElement(By.CssSelector("[data-tid='SendButton']"));
         sendButton.Click();
 
-        wait.Until(x => x.FindElements(By.CssSelector("[data-tid='MessageText']")).Last().Text == message);
+        var result = wait.Until(x => x.FindElements(By.CssSelector("[data-tid='MessageText']")).Last().Text == message);
+        
+        Assert.AreEqual(true,result,"Сообщение не отправилось");
     }
     
     [Test]
     public void WriteComment()
     {
+        /* Я думаю, что проблемы могут возникнуть потому, что тестовый стенд не откатывается до начального состояния.
+         * Могут произойти изменения у поста, под которым пишется коммент.
+         * Можно упереться в максимально допустимое количество комментариев под постом. 
+         */
+            
         var testComment = $"Test comment {Guid.NewGuid()}";
         driver.Navigate().GoToUrl(
             $"{staffUrl}/communities/18c18e9c-59dd-4d51-ad99-96261e81a5d3?tab=discussions&id=8c04521d-cc2e-488f-952d-30cb84f34a73");
@@ -107,7 +113,9 @@ public class HomeworkTests
         var sendComment = driver.FindElement(By.ClassName("react-ui-m0adju"));
         sendComment.Click();
 
-        wait.Until(x => x.FindElements(By.CssSelector("[data-tid='TextComment']")).Last().Text == testComment);
+        var result = wait.Until(x => x.FindElements(By.CssSelector("[data-tid='TextComment']")).Last().Text == testComment);
+        
+        Assert.AreEqual(true,result,"Комментарий не отправился");
     }
 
     [TearDown]
